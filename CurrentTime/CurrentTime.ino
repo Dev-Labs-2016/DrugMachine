dd322358858e98eaf1769e2a763ac0c23df2b3ee
AmrMahfouz <eng.csed.amr.mahfouz@gmail.com>
2015-11-29 20:46:09 +0200

    Get the current time example

---
+++ CurrentTime/CurrentTime.ino
@@ -0,0 +1,25 @@
+#include <Process.h>
+
+Process date;                 // process used to get the date
+
+void setup() {
+  Bridge.begin();        // initialize Bridge
+  Serial.begin(9600);    // initialize serial  
+
+  while(!Serial);               // wait for Serial Monitor to open
+
+  Serial.println("Time Check");  // Title of sketch
+}
+
+void loop() {
+  date.begin("/bin/date");
+  date.run();
+  
+  //if there's a result from the date process, get it.
+  while (date.available()>0) {
+    // print the results we got.
+    Serial.print(date.readString());
+  }
+  
+  delay(5000); // wait 5000ms
+}
